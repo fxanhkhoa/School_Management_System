998988d01f3caf59e8711df9edbfafd5d1b01c4d
fxanhkhoa <fxanhkhoa@gmail.com>
2019-12-06 18:53:23 +0700

    - adding 404 not found page
    Signed-off-by: fxanhkhoa <fxanhkhoa@gmail.com>

--- WorkProducts/School-Management-System/src/app/app.module.ts
+++ WorkProducts/School-Management-System/src/app/app.module.ts
@@ -1,18 +1,40 @@
+/* BrowserModule */
 import { BrowserModule } from '@angular/platform-browser';
+/* NgModule */
 import { NgModule } from '@angular/core';
+/* Mat Slider */
 import { MatSliderModule } from '@angular/material/slider';
+/* Router */
+import {RouterModule, Routes} from '@angular/router';
 
+/* App component */
 import { AppComponent } from './app.component';
+/* Browser Animations */
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+/* define Route */
+const appRoutes: Routes = [
+  {
+    path: '',
+    redirectTo: '/',
+    pathMatch: 'full'
+  }
+  {path: '**', component: PageNotFoundComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatSliderModule,
+    RouterModule.forRoot(
+      appRoutes
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
