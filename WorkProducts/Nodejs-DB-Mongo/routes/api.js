a21dfdab33f23d56e3bccd4d0afd65db1703bcff
fxanhkhoa <fxanhkhoa@gmail.com>
2019-12-19 02:00:42 +0700

    - doing with mongodb
    Signed-off-by: fxanhkhoa <fxanhkhoa@gmail.com>

--- WorkProducts/Nodejs-DB-Mongo/routes/api.js
+++ WorkProducts/Nodejs-DB-Mongo/routes/api.js
@@ -1,5 +1,23 @@
 const express = require('express')
 const router = express.Router()
+const User = require('../models/user')
+const mongoose = require('mongoose')
+const db = "mongodb://localhost:27017/school-management-system"
+
+mongoose.connect(db, 
+                  { 
+                     useNewUrlParser: true,
+                     useUnifiedTopology: true
+                  }, 
+                  err =>{
+   if (err){
+      console.error('Error!' + err)
+   } else {
+      console.log('Connected to mongodb')
+      // var query = User.find({email:'asdasd', password:'asdasd'})
+      // console.log(query)
+   }
+})
 
 router.get('/', (req, res) => {
    res.send('From API Route')
