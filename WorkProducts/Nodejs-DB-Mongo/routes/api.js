2d6f9cdfb6e447ee260fa861211dccbd3ef746cb
fxanhkhoa <fxanhkhoa@gmail.com>
2019-12-20 00:11:53 +0700

    register API
    - add design
    - add api register return json

--- WorkProducts/Nodejs-DB-Mongo/routes/api.js
+++ WorkProducts/Nodejs-DB-Mongo/routes/api.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const User = require('../models/user')
 const mongoose = require('mongoose')
-const db = "mongodb://localhost:27017/school-management-system"
+const db = "mongodb://myAdmin:myAdmin@localhost:6969/school-management-system"
 
 mongoose.connect(db, 
                   { 
@@ -21,6 +21,19 @@ mongoose.connect(db,
 
 router.get('/', (req, res) => {
    res.send('From API Route')
+})
+
+router.post('/register', (req, res) =>{
+   let userData = req.body
+   let user = new User(userData)
+
+   user.save((error, registeredUser) => {
+      if (error){
+         console.log(error)
+      } else {
+         res.status(200).send(registeredUser)
+      }
+   })
 })
 
 module.exports = router
