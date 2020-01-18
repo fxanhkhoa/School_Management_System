afe58116164c582d6ea6d74a51e2507c89bdcec2
fxanhkhoa <fxanhkhoa@gmail.com>
2020-01-19 00:39:07 +0700

    fix bug for student scheduler

--- WorkProducts/School-Management-System/src/app/Student/student-schedule/student-schedule.component.ts
+++ WorkProducts/School-Management-System/src/app/Student/student-schedule/student-schedule.component.ts
@@ -19,9 +19,8 @@ export class StudentScheduleComponent implements OnInit {
   _calendarType: String = 'CalendarTypeMonth';
   _calendarTypeDay = new CalendarTypeDay();
   _calendarTypeMonth = new CalendarTypeMonth();
-  _event = new Event();
-  allevents:Event[] = [];
 
+  // * Signal for UI loading
   loadedSignal: Boolean = false;
 
   /**
@@ -52,45 +51,43 @@ export class StudentScheduleComponent implements OnInit {
     var d = new Date();
     this._calendarTypeMonth.selectedMonth = d.getMonth() + 1; // ! Month is from 0 - 11
     this._calendarTypeMonth.selectedYear = d.getFullYear();
+    // TODO: Get all events
     this.getEvents();
-    console.log(this._calendarTypeMonth.events);
-    this.loadedSignal = true;
   }
 
 
-  getEvents(){
+  async getEvents(){
     let email = sessionStorage.getItem('email')
 
     // TODO: Call get all events of user API
     this._eventService.getEventsOfUser({email})
       .subscribe(
         res => {
-          this.allevents = res;
-          console.log(res)
+          // TODO: Check if res is empty then exit
+          if (res.length <= 0) return;
           for (let i = 0; i < res.length; i++){
-            this._event = new Event();
-            this._event.startdate = new Date(res[i].startdate);
-            this._event.enddate = new Date(res[i].enddate);
-            this._event.name = res[i].name;
-            this._event.content = res[i].content;
-            this._event.note = res[i].note;
-            this._event.progress = res[i].progress;
-            this._event.priority = res[i].priority;
-            this._event.type = res[i].type;
-            this._event.location = res[i].location;
+            let newEvent = new Event();
+            newEvent.startdate = new Date(res[i].startdate);
+            newEvent.enddate = new Date(res[i].enddate);
+            newEvent.name = res[i].name;
+            newEvent.content = res[i].content;
+            newEvent.note = res[i].note;
+            newEvent.progress = res[i].progress;
+            newEvent.priority = res[i].priority;
+            newEvent.type = res[i].type;
+            newEvent.location = res[i].location;
 
-            this.allevents.push(this._event);
-            // console.log(newEvent)
+            // TODO: Push to Events array
+            this._calendarTypeMonth.events.push(newEvent);
           }
+          // TODO: Signal to load UI
+          // ! Need to do here because this is asynchronous thread
+          // ! if no signal it will load first then get API data later
+          // ! So no data will be shown
+          this.loadedSignal = true;
         },
         err => console.log(err)
       )
-
-    console.log(this.allevents);
-    for (let i = 0; i < this.allevents.length; i++){
-      this._calendarTypeMonth.events.push(this.allevents[i]);
-    }
-    console.log(this._calendarTypeMonth.events);
   }
 
   /**
